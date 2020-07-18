0e3f9b71e17d4cf05d0aa05c4725401536a357e1
Cam <710180334@qq.com>
2020-07-18 23:25:38 +0800

    set pause when finished execute program

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
-%% build
+rem build
 "C:\Program Files\JetBrains\CLion 2020.1.1\bin\cmake\win\bin\cmake.exe" -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 16 2019" ./ -B build/vs_2019
 "C:\Program Files\JetBrains\CLion 2020.1.1\bin\cmake\win\bin\cmake.exe" --build D:\cam\c++\algorithm_cpp\build\vs_2019 --target algorithm_cpp --config Debug
+build\vs_2019\Debug\algorithm_cpp.exe
+pause
